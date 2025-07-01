1b93135576b8bc93cde4c17118211c9c6296ee36
maximilianus141 <maximilian.kenn@gmail.com>
2025-07-01 15:59:43 +0200

    fixed http-start using the wrong dir

--- http-start.sh
+++ http-start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# DO NOT RENAME THIS FILE
+
+cd /home/max/raspi
+
 flask --app main run -p 80 --host=0.0.0.0
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask as f
 
+
+def readFile(fileName):
+    with open(fileName) as f:
+        return f.read
+    
+
 app = f(__name__)
 
 @app.get("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
